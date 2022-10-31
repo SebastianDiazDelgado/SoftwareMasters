39e53fd5633006256b127805c2efbc661da057d4
SebastianDiazDelgado <sebsdiaz781@gmail.com>
2022-10-31 13:07:15 -0500

    first commit

---
+++ src/models/usuarios.model.ts
@@ -0,0 +1,64 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Usuarios extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  apellido: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  documento: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  correo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  telefono: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  rol: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  clave: string;
+
+
+  constructor(data?: Partial<Usuarios>) {
+    super(data);
+  }
+}
+
+export interface UsuariosRelations {
+  // describe navigational properties here
+}
+
+export type UsuariosWithRelations = Usuarios & UsuariosRelations;
